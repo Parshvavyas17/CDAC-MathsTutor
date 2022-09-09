41051deacb7a7f4dbc778abbfcaed369265620cd
Karna2701 <80153156+Karna2701@users.noreply.github.com>
2022-09-09 14:03:56 +0530

    Message text

--- src/components/Polynomial.js
+++ src/components/Polynomial.js
@@ -6,41 +6,41 @@ export default function Polynomial() {
         <h2 className='header'><b className="text">Polynomial Equation:</b></h2>
         <div className='my-20'>
             <form action="/">
-                <label htmlFor="noofterms" className='my-3 mx-2'>
-                    Enter the number of terms: 
-                </label>
-                <input className= "mx-5" style={{width: '50px'}} type="number" />
+          
+               
                 <br />
-                <label htmlFor="noofterms" className='my-3 mx-2'>
-                    Enter the Equation: 
+                <label htmlFor="noofterms" className='my-1 mx-2'>
+                   The Equation to be integrated : <label className='my-1'> <h5>3x<sup>2</sup> - 2x + 5</h5> </label>
                 </label>
-                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px'}} type="number" />
-                <sup><input type="number" style={{width: '40px'}}/></sup>
-                +
-                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px'}} type="number" />
-                <sup><input type="number" style={{width: '40px'}}/></sup>
-                +
-                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px'}} type="number" />
-                <sup><input type="number" style={{width: '40px'}}/></sup>
-                <br />
-                <label htmlFor="noofterms" className='my-3 mx-2'>
-                    Enter the Upper Limit: 
+                <br/>
+                <br/>
+                
+               
+                <label htmlFor="noofterms" className='my-6 mx-2'>
+                   The upper limit is : <label className='my-6'> <h5> 2 </h5> </label>
                 </label>
-                <input className= "mx-5" style={{width: '50px'}} type="number" />
-                <br />
-                <label htmlFor="noofterms" className='my-3 mx-2'>
-                    Enter the Lower Limit: 
+                <br/>
+                <br/>
+                
+               
+                <label htmlFor="noofterms" className='my-6 mx-2'>
+                The lower limit is : <label className='my-6'> <h5> 1 </h5> </label>
                 </label>
-                <input className= "mx-5" style={{width: '50px'}} type="number" />
-                <br />
-                <label htmlFor="noofterms" className='my-3 mx-2'>
+                <br/>
+                <br/>
+                <label htmlFor="noofterms" className='my-6 mx-2'>
                     The Area Under the Curve is:  
+
                 </label>
-                <input className= "mx-5" style={{width: '50px'}} type="number" />
+                <input className= "mx-3" style={{width: '50px'}} type="number" />
                 <br />
+                
+                
+                
+             
             </form>
-            <a href="/r1"><button className='btn btn-outline-success my-3 mx-10' style={{height: '75px', width: '100px',borderRadius: '50%', textAlign: 'center', marginLeft: '75px'}}>Right</button></a>
-            <a href="/w1"><button className='btn btn-outline-success my-3 mx-10' style={{height: '75px', width: '100px',borderRadius: '50%', textAlign: 'center', marginLeft: '75px'}}>Wrong</button></a>
+            <a href="/r1"><button className='btn btn-outline-success my-3 mx-10' style={{height: '75px', width: '100px',borderRadius: '50%', textAlign: 'center', marginLeft: '75px'}}>Submit</button></a>
+           
         </div>
 
     </>
