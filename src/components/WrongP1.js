8c9ea62869886f3da6a706db918a045d2bf1ea23
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2022-10-30 16:25:31 +0530

    wrong 1 changes

--- src/components/WrongP1.js
+++ src/components/WrongP1.js
@@ -1,6 +1,86 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function WrongP1() {
+  const navigator = useNavigate();
+    const [ upCo1, setUpCo1 ] = useState(null);
+    const handleUpCo1 = (e) => {
+        setUpCo1(e.target.value);
+    }
+    const [ upCo2, setUpCo2 ] = useState(null);
+    const handleUpCo2 = (e) => {
+        setUpCo2(e.target.value);
+    }
+    const [ upCo3, setUpCo3 ] = useState(null);
+    const handleUpCo3 = (e) => {
+        setUpCo3(e.target.value);
+    }
+    const [ upPo1, setUpPo1 ] = useState(null);
+    const handleUpPo1 = (e) => {
+        setUpPo1(e.target.value);
+    }
+    const [ upPo2, setUpPo2 ] = useState(null);
+    const handleUpPo2 = (e) => {
+        setUpPo2(e.target.value);
+    }
+    const [ upPo3, setUpPo3 ] = useState(null);
+    const handleUpPo3 = (e) => {
+        setUpPo3(e.target.value);
+    }
+    // const [ loCo1, setLoCo1 ] = useState(null);
+    // const handleLoCo1 = (e) => {
+    //     setLoCo1(e.target.value);
+    // }
+    // const [ loCo2, setLoCo2 ] = useState(null);
+    // const handleLoCo2 = (e) => {
+    //     setLoCo2(e.target.value);
+    // }
+    // const [ loCo3, setLoCo3 ] = useState(null);
+    // const handleLoCo3 = (e) => {
+    //     setLoCo3(e.target.value);
+    // }
+    // const [ loPo1, setLoPo1 ] = useState(null);
+    // const handleLoPo1 = (e) => {
+    //     setLoPo1(e.target.value);
+    // }
+    // const [ loPo2, setLoPo2 ] = useState(null);
+    // const handleLoPo2 = (e) => {
+    //     setLoPo2(e.target.value);
+    // }
+    // const [ loPo3, setLoPo3 ] = useState(null);
+    // const handleLoPo3 = (e) => {
+    //     setLoPo3(e.target.value);
+    // }
+
+    const handleSubmit = (e) => {
+        if(!upCo1 || !upCo2 || !upCo3 || !upPo1 || !upPo2 || !upPo3) {
+            alert('Enter all the values!');
+            return;
+        }
+        console.log(upCo1, upCo2, upCo3, upPo1, upPo2, upPo3);
+        if(upCo1 == 1 && upPo1 == 3 &&  upCo2 == 1 && upPo2 == 2 && upCo3 == 5 && upPo3 == 1) {
+            alert('Right Answer!!!');
+            navigator('/r1');
+        }
+        else {
+            alert('Wrong Answer!!!');
+            setUpCo1(null);
+            setUpPo1(null);
+            setUpCo2(null);
+            setUpPo2(null);
+            setUpCo3(null);
+            setUpPo3(null);
+            // setLoCo1(null);
+            // setLoPo1(null);
+            // setLoCo2(null);
+            // setLoPo2(null);
+            // setLoCo3(null);
+            // setLoPo3(null);
+            navigator('/w3');
+        }
+    }
+    
   return (
     <>
         <header style={{textAlign: 'center',  whiteSpace: "nowrap", display: 'inline'}}>
@@ -11,8 +91,21 @@ export default function WrongP1() {
             <h5>3x<sup>2</sup> - 2x + 5</h5>
 
             <h3>Integrated Equation: </h3> 
-            <h5>x<sup>3</sup> - x<sup>2</sup> + 5x</h5>
-            <a href="/w2"><button className='btn btn-outline-success my-3 mx-10' style={{height: '75px', width: '100px',borderRadius: '50%', textAlign: 'center', marginLeft: '25px'}}>Next</button></a>
+
+            (
+                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px', marginTop: '8px'}} type="number" name='upCo1' value={upCo1} onChange={handleUpCo1} />
+                <sup><input type="number" style={{width: '40px'}} name='upPo1' value={upPo1} onChange={handleUpPo1} /></sup>
+                +
+                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px'}} type="number" name='upCo2' value={upCo2} onChange={handleUpCo2} />
+                <sup><input type="number" style={{width: '40px'}} name='upPo2' value={upPo2} onChange={handleUpPo2} /></sup>
+                +
+                <input className= "" style={{width: '50px',marginLeft: '10px', marginRight: '2px'}} type="number" name='upCo3' value={upCo3} onChange={handleUpCo3} />
+                <sup><input type="number" style={{width: '40px'}} name='upPo3' value={upPo3} onChange={handleUpPo3} /></sup>)
+                -
+                <br />
+                <button className='btn btn-outline-success my-3 mx-10' style={{height: '75px', width: '100px',borderRadius: '50%', textAlign: 'center', marginTop: '5px'}} onClick={handleSubmit} >Submit</button>
+                <br />
+
         </section>
       
     </>
