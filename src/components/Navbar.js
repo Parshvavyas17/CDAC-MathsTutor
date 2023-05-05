2bb0fd068898f50efb39e15d4cf598b37bf0c3db
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-05-05 16:27:46 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,6 +41,11 @@ const Navbar = () => {
                 Exponential
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/mixed">
+                Mixed
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
